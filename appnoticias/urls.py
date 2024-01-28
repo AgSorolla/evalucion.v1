1fcb26736b923a0791c5c897287c1bde3b10c54e
Matias Martinez <mat.martinezb@duocuc.cl>
2024-01-28 16:22:49 -0300

    Modificado el login y register, ahora todas las paginas se pueden ver bien si las seleccionamos desde el index, falta agregar modelos etc.

--- appnoticias/urls.py
+++ appnoticias/urls.py
@@ -6,5 +6,10 @@ from . import views
 
 urlpatterns=[
     path('index/',views.index,name='index'),
+    path('login/',views.login,name='login'),
+    path('mas_noticias/',views.mas_noticias,name='mas_noticias'),
+    path('noticia.html/',views.noticia,name='noticia'),
+    path('register/',views.register,name='register'),
+    path('sobre_nosotros/',views.sobre_nosotros,name='sobre_nosotros'),
     
 ]
--- appnoticias/views.py
+++ appnoticias/views.py
@@ -6,3 +6,30 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request, 'appnoticias/index.html', context)
+
+def login(request):
+    context={}
+    return render(request, 'appnoticias/login.html')
+
+
+def mas_noticias(request):
+    context={}
+    return render(request, 'appnoticias/mas_noticias.html')
+
+
+def noticia(request):
+    context={}
+    return render(request, 'appnoticias/noticia.html')
+
+
+def register(request):
+    context={}
+    return render(request, 'appnoticias/register.html')
+
+
+def sobre_nosotros(request):
+    context={}
+    return render(request, 'appnoticias/sobre_nosotros.html')
+
+
+
