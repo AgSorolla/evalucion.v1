75802cd95c3c4105c6a45218633315393fca2165
Matias Martinez <mat.martinezb@duocuc.cl>
2024-01-30 18:54:46 -0300

    ARREGLADO FALTA CRUD

--- appnoticias/urls.py
+++ appnoticias/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views 
+  # Importa la vista 'index'
 
 
-
+app_name = 'appnoticias'
 
 urlpatterns=[
     path('index/',views.index,name='index'),
-    path('login/',views.login,name='login'),
+    path('login/',views.login_view,name='login'),
     path('mas_noticias/',views.mas_noticias,name='mas_noticias'),
     path('noticia.html/',views.noticia,name='noticia'),
     path('register/',views.register,name='register'),
--- appnoticias/views.py
+++ appnoticias/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 
 def index(request):
     context={}
-    return render(request, 'appnoticias/index.html', context)
+    return render(request, 'appnoticias/index.html')
 
-def login(request):
+def login_view(request):
     context={}
     return render(request, 'appnoticias/login.html')
 
@@ -31,5 +35,22 @@ def sobre_nosotros(request):
     context={}
     return render(request, 'appnoticias/sobre_nosotros.html')
 
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('appnoticias:index')  # Corregir aquí
+            else:
+                messages.error(request, 'Credenciales inválidas. Por favor, inténtalo de nuevo.')     
+                
+    else:
+        form = LoginForm()
+
+    return render(request, 'appnoticias/login.html', {'form': form})
 
 
